f405a23c4551151ae4a37f15208a179bfb993f62
Moeko3737 <aktrs825@gmail.com>
2026-09-03 10:11:33 +0900

    タイトル画面の作成

---
+++ tests/titleScreen.test.js
@@ -0,0 +1,31 @@
+import test from "node:test";
+import assert from "node:assert/strict";
+import { access, readFile } from "node:fs/promises";
+
+const projectRoot = new URL("../", import.meta.url);
+
+test("タイトル画面がロゴとOP背景を使用する", async () => {
+  const [html, css] = await Promise.all([
+    readFile(new URL("index.html", projectRoot), "utf8"),
+    readFile(new URL("css/style.css", projectRoot), "utf8")
+  ]);
+
+  assert.match(html, /id="title-screen"/);
+  assert.match(html, /assets\/images\/ui\/title-logo\.png/);
+  assert.match(css, /assets\/images\/backgrounds\/op-campus\.png/);
+});
+
+test("PC表示用の左右パネルが用意されている", async () => {
+  const html = await readFile(new URL("index.html", projectRoot), "utf8");
+
+  assert.match(html, /desktop-side--left/);
+  assert.match(html, /desktop-side--right/);
+  assert.match(html, /side-year-card/);
+});
+
+test("タイトル画面で使用する画像ファイルが存在する", async () => {
+  await Promise.all([
+    access(new URL("assets/images/ui/title-logo.png", projectRoot)),
+    access(new URL("assets/images/backgrounds/op-campus.png", projectRoot))
+  ]);
+});
